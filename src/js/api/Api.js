82b1f8ca7c0a7808ea39b868ef5876bce6bc2534
AndreyS55 <storozhko-a-v@mail.ru>
2018-09-16 21:12:59 +0300

    add history push

--- src/js/api/Api.js
+++ src/js/api/Api.js
@@ -10,11 +10,21 @@ const urlConverter = (values= {}, page = 1) => {
     return `${API_URL}?api-key=${API_KEY}${query}${startDate}${endDate}${pageCorresponds}`;
 };
 
+const historyConverter = (values= {}, page = 1) => {
+    let query = values.searchText ? '&query=' + values.searchText.split(' ').join('+') : '';
+    let startDate = values.startDate ? '&begin_date=' + values.startDate.split('-').join('') : '';
+    let endDate = values.endDate ? '&end_date=' + values.endDate.split('-').join('') : '';
+    let pageCorresponds = '&page=' + page;
+    return `/?${query}${startDate}${endDate}${pageCorresponds}`;
+};
+
 const NYTApi = (values, page) => {
-  return fetch(urlConverter(values, page))
-      .then(function (response) {
-          return response.json()
-      })
+    let historyParams = historyConverter(values, page);
+    history.pushState(null, "", `${historyParams}`);
+    return fetch(urlConverter(values, page))
+    .then(function (response) {
+      return response.json()
+    })
 };
 
 export default NYTApi;
--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { createBrowserHistory } from 'history';
 import configureStore from '../../configs/configureStore';
 import Container from '../Container/Container'
 import './App.scss';
 
 const store = configureStore();
+const browserHistory = createBrowserHistory();
+const history = syncHistoryWithStore(browserHistory, store);
+
 const App = () => (
     <Provider store={store}>
-        <Router>
+        <Router history={history}>
             <Route path='/:filter?' component={Container}/>
         </Router>
     </Provider>
